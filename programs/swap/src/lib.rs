84f873fc5d3a335d744595579912ff4c9f5e7478
Osaretin Frank <osaretin.frank10@gmail.com>
2025-07-14 08:21:27 +0100

    init

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("FJMGnEKVQqcZbVHcXLJaFCPw6KZpv4N7kRh3u4GhkZ26");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
